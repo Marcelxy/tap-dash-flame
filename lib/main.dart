51eece8d7d6753108f22767ab8bbe33c2add81f2
Tobias <tma@idea-invest.de>
2019-11-24 17:19:51 +0100

    first working flame version

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,60 @@
+import 'dart:ui';
+
+import 'package:flame/anchor.dart';
+import 'package:flame/components/text_box_component.dart';
+import 'package:flame/components/text_component.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flame/palette.dart';
+import 'package:flame/text_config.dart';
 import 'package:flutter/material.dart';
 
-//void main() => runApp(MyApp());
+void main() async {
+  final Size size = await Flame.util.initialDimensions();
+  runApp(MyGame(size).widget);
+}
 
-class MyAppHome extends StatelessWidget {
-  // This widget is the root of your application.
+TextConfig regular = TextConfig(color: BasicPalette.white.color);
+TextConfig tiny = regular.withFontSize(12.0);
+
+class MyTextBox extends TextBoxComponent {
+  MyTextBox(String text)
+      : super(text,
+            config: tiny, boxConfig: const TextBoxConfig(timePerChar: 0.05));
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  void drawBackground(Canvas c) {
+    final Rect rect = Rect.fromLTWH(0, 0, width, height);
+    c.drawRect(rect, Paint()..color = const Color(0xFFFF00FF));
+    c.drawRect(
+        rect.deflate(boxConfig.margin),
+        Paint()
+          ..color = BasicPalette.black.color
+          ..style = PaintingStyle.stroke);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyGame extends BaseGame {
+  MyGame(Size screenSize) {
+    size = screenSize;
+    add(TextComponent('Hello, Flame', config: regular)
+      ..anchor = Anchor.topCenter
+      ..x = size.width / 2
+      ..y = 32.0);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    add(TextComponent('center', config: tiny)
+      ..anchor = Anchor.center
+      ..x = size.width / 2
+      ..y = size.height / 2);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    add(TextComponent('bottomRight', config: tiny)
+      ..anchor = Anchor.bottomRight
+      ..x = size.width
+      ..y = size.height);
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    add(MyTextBox(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum.')
+      ..anchor = Anchor.bottomLeft
+      ..y = size.height);
   }
 }
