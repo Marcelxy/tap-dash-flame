dd2024d1824fd3b1030402039b05b9e691641c40
Marcelxy <marcel.geirhos@gmail.com>
2019-11-28 22:25:34 +0100

    Google Login verbessert
    - Google Login implementiert
    - Google Anmeldebutton erstellt mit flutter_auth_buttons Plugin
    - Facebook Anmeldebutton erstellt mit flutter_auth_buttons Plugin (noch kein Facebook Login implementiert)
    - registerPage screenOrientation auf portrait beschränkt
    - Nicht mehr benötigte Dateien entfernt.

--- lib/registerPage.dart
+++ lib/registerPage.dart
@@ -1,8 +1,8 @@
 import 'main.dart';
 import 'package:flutter/material.dart';
-import 'package:play_games/play_games.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,9 +22,8 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   @override
   Widget build(BuildContext context) {
@@ -35,124 +34,65 @@ class _RegisterPageState extends State<RegisterPage> {
       body: Container(
         color: Colors.white,
         child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: FlutterLogo(size: 150),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: FlutterLogo(size: 150),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: GoogleSignInButton(
+                  onPressed: () {
+                    _signInWithGoogle().catchError((e) => print(e));
+                  },
+                  darkMode: true,
+                  borderRadius: 10,
+                  text: 'Anmelden mit Google',
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: _playGamesRegistrationButton(),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: FacebookSignInButton(
+                  onPressed: () {},
+                  borderRadius: 10,
+                  text: 'Anmelden mit Facebook',
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: _googleRegistrationButton(),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 
-  Widget _playGamesRegistrationButton() => OutlineButton(
-    splashColor: Colors.grey,
-    onPressed: () {},
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-    highlightElevation: 5,
-    borderSide: BorderSide(color: Colors.grey),
-    child: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(image: AssetImage("assets/images/google_play_games_logo.png"), height: 25),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text(
-                'Anmelden mit Play Games',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.grey,
-                ),
-              ),
-            )
-          ],
-        ),
-    ),
-  );
-
-  Widget _googleRegistrationButton() => OutlineButton(
-    splashColor: Colors.grey,
-    onPressed: () {
-      signInWithGoogle().whenComplete(() {
-        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-          return MyAppHome();
-        }));
-      });
-    },
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-    highlightElevation: 5,
-    borderSide: BorderSide(color: Colors.grey),
-    child: Padding(
-      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image(image: AssetImage("assets/images/google_logo.png"), height: 20),
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Text(
-              'Anmelden mit Google',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.grey,
-              ),
-            ),
-          )
-        ],
-      )
-    ),
-  );
-
-  // TODO
-  void _playGamesRegistration(BuildContext context) async {
-    SigninResult result = await PlayGames.signIn();
-    if (result.success) {
-      await PlayGames.setPopupOptions();
-    } else {
-      // TODO Fehlermeldung ausgeben!
-    }
-  }
-
-  Future<String> signInWithGoogle() async {
-    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-    final GoogleSignInAuthentication googleSignInAuthentication =
-    await googleSignInAccount.authentication;
+  Future<FirebaseUser> _signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
 
     final AuthCredential credential = GoogleAuthProvider.getCredential(
-        accessToken: googleSignInAuthentication.accessToken,
-        idToken: googleSignInAuthentication.idToken,
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
     );
 
-    final AuthResult authResult = await _auth.signInWithCredential(credential);
-    final FirebaseUser user = authResult.user;
-
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-
-    final FirebaseUser currentUser = await _auth.currentUser();
-    assert(user.uid == currentUser.uid);
-
-    return 'signInWithGoogle succeeded: $user';
+    final FirebaseUser user =
+        (await _auth.signInWithCredential(credential)).user;
+    if (user != null) {
+      print("Angemeldet über Google mit " + user.displayName);
+      print(user);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => MyAppHome()),
+      );
+      return user;
+    }
+    return null;
   }
 
   void signOutGoogle() async {
-    await googleSignIn.signOut();
-    print("User Sign Out");
+    await _googleSignIn.signOut();
+    print("Spieler hat sich ausgeloggt.");
   }
 }
